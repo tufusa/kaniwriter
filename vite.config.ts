b46d8c53075e398bd4b4e1398f71d5d121335b2b
tufusa <tufusa74@gmail.com>
2023-11-01 16:18:57 +0900

    chore: パスエイリアスを設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "src/components"),
+      css: path.resolve(__dirname, "src/css"),
+      pages: path.resolve(__dirname, "src/pages"),
+      routes: path.resolve(__dirname, "src/routes"),
+    },
+  },
+});
