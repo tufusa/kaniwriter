8a239aa320b557dfa8257c0befef9ae8b19b64d0
kiharu3112 <kiharu3112@gmail.com>
2023-11-01 15:57:54 +0900

    fileアップロードを消した or other

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import {
   Box,
   Button,
-  SvgIcon,
   FormLabel,
   Radio,
   radioClasses,
@@ -10,19 +9,14 @@ import {
   Sheet,
   styled,
 } from "@mui/joy";
-import { Step, StepLabel, Stepper, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import "../css/home.css";
 
 import Rboard from "/public/Rboard.png";
 import ESP32 from "/public/ESP32.png";
 import { Flag, Usb } from "@mui/icons-material";
 import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
-const steps = [
-  "マイコン選択",
-  "ファイルのアップロード",
-  "マイコン接続",
-  "書き込み中",
-];
+import SerialProvider, { useSerial } from "./SerialProvider";
 
 const targets = [
   {
@@ -35,29 +29,46 @@ const targets = [
   },
 ];
 
-const VisuallyHiddenInput = styled('input')`
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  white-space: nowrap;
-  width: 1px;
-`;
+
 
 export function Home() {
   const [step, setStep] = useState<number>(0);
   const [target, setTarget] = useState<string>("");
-  const [file, setFile] = useState<File | null>(null);
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files) {
-      setFile(files[0]);
-      if (step === 1) setStep(2);
+  let port;
+  const [baud, setBaud] = useState<number>(115200);
+  async function onConnectButtonClick() {
+    try {
+      port = await navigator.serial.requestPort();
+      await port.open({ baudRate: baud });
+
+      while (true) {
+        const reader = port.readable.getReader();
+        try {
+          while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+              break;
+            }
+            console.log(value);
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          reader.releaseLock();
+        }
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
+
+  async function sendSerial() {
+    const encoder = new TextEncoder();
+    const writer = port.writable.getWriter();
+    await writer.write(encoder.encode("Hello World"));
+    writer.releaseLock();
+  }
+
   return (
     <div id={"home"}>
       <Box id={"home"}>
@@ -70,24 +81,7 @@ export function Home() {
           書き込みツール
         </Typography>
 
-        { /* ステップ */}
-        <Stepper
-          activeStep={step}
-          alternativeLabel
-          sx={{ width: 900, margin: "3rem auto" }}
-        >
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>
-                <Typography fontFamily={"'M PLUS Rounded 1c', sans-serif"}>
-                  {label}
-                </Typography>
-              </StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-
-        { /* マイコン選択 */}
+        {/* マイコン選択 */}
         <Box sx={{ display: "flex", gap: 2 }}>
           <RadioGroup
             aria-label="platform"
@@ -139,12 +133,14 @@ export function Home() {
                   checkedIcon={<CheckCircleRoundedIcon />}
                   checked={target === value.title}
                   onChange={(e) => {
-                    setTarget(e.target.value)
+                    setTarget(e.target.value);
                     if (step === 0) setStep(1);
                   }}
                 />
                 <FormLabel htmlFor={value.title}>
-                  <Typography fontFamily={"'M PLUS Rounded 1c', sans-serif"}>{value.title}</Typography>
+                  <Typography fontFamily={"'M PLUS Rounded 1c', sans-serif"}>
+                    {value.title}
+                  </Typography>
                 </FormLabel>
                 <img
                   src={value.image}
@@ -160,49 +156,24 @@ export function Home() {
           </RadioGroup>
         </Box>
 
-        { /* ファイルのアップロード */}
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          component="label"
-          role={undefined}
-          tabIndex={-1}
-          variant="outlined"
-          color="neutral"
-          startDecorator={
-            <SvgIcon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                />
-              </svg>
-            </SvgIcon>
-          }
-        >
-          Upload a file
-          <VisuallyHiddenInput type="file" onChange={handleFileChange} />
-        </Button>
-        </Box>
+        {/* マイコン接続 */}
 
-        { /* マイコン接続 */}
         <Box sx={{ display: "flex", justifyContent: "center", margin: "1rem" }}>
-          <Button disabled={step !== 2} onClick={() => {if (step === 2) setStep(3)}}>
+          <Button
+            //disabled={step !== 2}
+            onClick={() => {
+              if (step === 2) setStep(3);
+              onConnectButtonClick();
+            }}
+          >
             接続
             <Usb />
           </Button>
         </Box>
 
-
-        { /* 書き込み中 */}
+        {/* 書き込み中 */}
         <Box sx={{ display: "flex", justifyContent: "center", margin: "1rem" }}>
-          <Button disabled={step !== 3}>
+          <Button disabled={step !== 3} onClick={sendSerial}>
             書き込み
             <Flag />
           </Button>
