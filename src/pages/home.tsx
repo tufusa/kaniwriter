37a62ae41cc00185338ded80f148be358c1bb23c
tufusa <tufusa74@gmail.com>
2023-11-22 16:20:33 +0900

    feat: コンパイラと連携

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,19 +8,32 @@ import {
   RadioGroup,
   Sheet,
 } from "@mui/joy";
-import { Input, Typography } from "@mui/material";
+import {
+  Checkbox,
+  CircularProgress,
+  FormControlLabel,
+  Input,
+  Typography,
+} from "@mui/material";
+import {
+  Flag as FlagIcon,
+  Usb as UsbIcon,
+  CheckCircleRounded as CheckCircleRoundedIcon,
+  Check as CheckIcon,
+  ErrorOutline as ErrorOutlineIcon,
+} from "@mui/icons-material";
 import Ansi from "ansi-to-react";
+import Base64 from "base64-js";
+
+import { MrubyWriterConnector, Target } from "libs/mrubyWriterConnector";
+import { isTarget } from "libs/utility";
 import { useQuery } from "src/hooks/useQuery";
+import { compiler } from "libs/axios";
+import RBoard from "/Rboard.png";
+import ESP32 from "/ESP32.png";
 import "css/home.css";
 
-import RBoard from "/RBoard.png";
-import ESP32 from "/ESP32.png";
-import { Flag, Usb } from "@mui/icons-material";
-import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
-import { MrubyWriterConnector, Target } from "../libs/mrubyWriterConnector";
-import { isTarget } from "../libs/utility";
-
-const targets: Array<{ title: Target; image: string }> = [
+const targets = [
   {
     title: "RBoard",
     image: RBoard,
@@ -29,17 +42,26 @@ const targets: Array<{ title: Target; image: string }> = [
     title: "ESP32",
     image: ESP32,
   },
-];
+] as const satisfies readonly { title: Target; image: string }[];
+
+type CompileStatus = {
+  status: "idle" | "compile" | "success" | "error";
+  error?: string;
+};
 
 export const Home = () => {
   const query = useQuery();
   const id = query.get("id");
+
   const targetItem = localStorage.getItem("target");
   const [target, setTarget] = useState<Target>(
     isTarget(targetItem) ? targetItem : "RBoard"
   );
-  const [command, setCommand] = useState("");
-  const [log, setLog] = useState<string[]>([]);
+  const autoConnectItem = localStorage.getItem("autoConnect");
+  const [autoConnectMode, setAutoConnectMode] = useState<boolean>(
+    autoConnectItem === "true"
+  );
+
   const [connector] = useState<MrubyWriterConnector>(
     new MrubyWriterConnector({
       target,
@@ -48,17 +70,68 @@ export const Home = () => {
       useAnsi: true,
     })
   );
+  const [command, setCommand] = useState("");
+  const [log, setLog] = useState<string[]>([]);
   const [code, setCode] = useState<Uint8Array>();
+  const [compileStatus, setCompileStatus] = useState<CompileStatus>({
+    status: "idle",
+  });
 
   useEffect(() => {
-    (async () => {
-      await fetch("./main.mrb").then(async (res) => {
-        if (!res.body) return;
+    const compile = async () => {
+      setCompileStatus({ status: "idle" });
 
-        const { value } = await res.body?.getReader().read();
-        setCode(value);
-      });
-    })();
+      const code = await compiler
+        .get(`/code/${id}`)
+        .then((res) => res.data as { code: string })
+        .catch(() => undefined);
+      if (!code) {
+        setCompileStatus({
+          status: "error",
+          error: "No source code found.",
+        });
+        return;
+      }
+
+      setCompileStatus({ status: "compile" });
+
+      compiler
+        .post(`/code/${id}/compile`)
+        .then((res) => {
+          const result = res.data as { binary: string; error: string };
+          if (result.error !== "") return;
+
+          setCode(Base64.toByteArray(result.binary));
+          setCompileStatus({ status: "success" });
+        })
+        .catch(() =>
+          setCompileStatus({ status: "error", error: "Compile failed." })
+        );
+    };
+
+    compile();
+  }, []);
+
+  useEffect(() => {
+    if (!autoConnectMode) return;
+
+    const autoConnect = async () => {
+      const ports = await navigator.serial.getPorts();
+      if (ports.length == 0) return;
+
+      connector
+        .connect(async () => ports[0])
+        .then((result) => {
+          if (result.isFailure()) {
+            console.log(result);
+            return;
+          }
+
+          read();
+        });
+    };
+
+    autoConnect();
   }, []);
 
   const connect = async () => {
@@ -105,122 +178,184 @@ export const Home = () => {
   };
 
   return (
-    <div id={"home"}>
-      <Box id={"home"}>
-        <Typography
-          variant="h4"
-          component="div"
-          color="black"
-          fontFamily={"'M PLUS Rounded 1c', sans-serif"}
+    <Box id="home">
+      <Typography
+        variant="h4"
+        component="div"
+        color="black"
+        fontFamily={"'M PLUS Rounded 1c', sans-serif"}
+      >
+        書き込みツール
+      </Typography>
+
+      <CompileStatusCard status={compileStatus} />
+
+      {/* マイコン選択 */}
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <RadioGroup
+          aria-label="platform"
+          defaultValue="Website"
+          overlay
+          name="platform"
+          sx={{
+            flexDirection: "row",
+            margin: "2rem auto",
+            gap: 2,
+            [`& .${radioClasses.checked}`]: {
+              [`& .${radioClasses.action}`]: {
+                inset: -1,
+                border: "3px solid",
+                borderColor: "primary.500",
+              },
+            },
+            [`& .${radioClasses.radio}`]: {
+              display: "contents",
+              "& > svg": {
+                zIndex: 2,
+                position: "absolute",
+                top: "-8px",
+                right: "-8px",
+                bgcolor: "background.surface",
+                borderRadius: "50%",
+              },
+            },
+          }}
         >
-          書き込みツール
-        </Typography>
-
-        {/* マイコン選択 */}
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <RadioGroup
-            aria-label="platform"
-            defaultValue="Website"
-            overlay
-            name="platform"
-            sx={{
-              flexDirection: "row",
-              margin: "2rem auto",
-              gap: 2,
-              [`& .${radioClasses.checked}`]: {
-                [`& .${radioClasses.action}`]: {
-                  inset: -1,
-                  border: "3px solid",
-                  borderColor: "primary.500",
-                },
-              },
-              [`& .${radioClasses.radio}`]: {
-                display: "contents",
-                "& > svg": {
-                  zIndex: 2,
-                  position: "absolute",
-                  top: "-8px",
-                  right: "-8px",
-                  bgcolor: "background.surface",
-                  borderRadius: "50%",
-                },
-              },
-            }}
-          >
-            {targets.map((value, index) => (
-              <Sheet
-                key={index}
-                variant="outlined"
-                sx={{
-                  borderRadius: "md",
-                  boxShadow: "sm",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  gap: 1.5,
-                  p: 2,
-                  minWidth: 120,
+          {targets.map((value, index) => (
+            <Sheet
+              key={index}
+              variant="outlined"
+              sx={{
+                borderRadius: "md",
+                boxShadow: "sm",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 1.5,
+                p: 2,
+                minWidth: 120,
+              }}
+            >
+              <Radio
+                id={value.title}
+                value={value.title}
+                checkedIcon={<CheckCircleRoundedIcon />}
+                checked={target === value.title}
+                onChange={() => {
+                  setTarget(value.title);
+                  connector.setTarget(value.title);
+                  localStorage.setItem("target", value.title);
                 }}
-              >
-                <Radio
-                  id={value.title}
-                  value={value.title}
-                  checkedIcon={<CheckCircleRoundedIcon />}
-                  checked={target === value.title}
-                  onChange={() => {
-                    setTarget(value.title);
-                    connector.setTarget(value.title);
-                    localStorage.setItem("target", value.title);
-                    if (step === 0) setStep(1);
-                  }}
-                />
-                <FormLabel htmlFor={value.title}>
-                  <Typography fontFamily={"'M PLUS Rounded 1c', sans-serif"}>
-                    {value.title}
-                  </Typography>
-                </FormLabel>
-                <img
-                  src={value.image}
-                  alt={value.title}
-                  style={{
-                    aspectRatio: "1/1",
-                    width: "10rem",
-                    margin: "0 auto",
-                  }}
-                />
-              </Sheet>
-            ))}
-          </RadioGroup>
-        </Box>
-
-        {/* マイコン接続 */}
-
-        <Box sx={{ display: "flex", justifyContent: "center", margin: "1rem" }}>
-          <Button
-            //disabled={step !== 2}
-            onClick={connect}
-          >
-            接続
-            <Usb />
-          </Button>
-        </Box>
-
-        <Log log={log} />
-        <Input type="text" onChange={(e) => setCommand(e.target.value)} />
-        <Input type="submit" onClick={() => send(command)} value="Send" />
-
-        {/* 書き込み中 */}
-        <Box sx={{ display: "flex", justifyContent: "center", margin: "1rem" }}>
-          <Button onClick={writeCode}>
-            書き込み
-            <Flag />
-          </Button>
-        </Box>
-        <Button onClick={() => setStep(0)}>リセット</Button>
+              />
+              <FormLabel htmlFor={value.title}>
+                <Typography fontFamily={"'M PLUS Rounded 1c', sans-serif"}>
+                  {value.title}
+                </Typography>
+              </FormLabel>
+              <img
+                src={value.image}
+                alt={value.title}
+                style={{
+                  aspectRatio: "1/1",
+                  width: "10rem",
+                  margin: "0 auto",
+                }}
+              />
+            </Sheet>
+          ))}
+        </RadioGroup>
       </Box>
-    </div>
+
+      {/* マイコン接続 */}
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: "1rem",
+          justifyContent: "center",
+          margin: "1rem",
+        }}
+      >
+        <Button onClick={connect}>
+          接続
+          <UsbIcon />
+        </Button>
+        <Button onClick={writeCode}>
+          書き込み
+          <FlagIcon />
+        </Button>
+      </Box>
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(ev) => {
+              const checked = ev.currentTarget.checked;
+              setAutoConnectMode(checked);
+              localStorage.setItem("autoConnect", `${checked}`);
+
+              if (checked) window.location.reload();
+            }}
+            checked={autoConnectMode}
+          />
+        }
+        label="Auto connect"
+        sx={{ color: "black" }}
+      />
+
+      <Log log={log} />
+      <Input type="text" onChange={(e) => setCommand(e.target.value)} />
+      <Input type="submit" onClick={() => send(command)} value="Send" />
+    </Box>
   );
 };
+
+const CompileStatusCard = (props: { status: CompileStatus }) => (
+  <Sheet
+    variant="outlined"
+    color="neutral"
+    sx={{
+      m: "1rem auto 0 auto",
+      p: "0.5rem 1.5rem",
+      width: "15rem",
+      borderRadius: "sm",
+      display: "flex",
+      flexWrap: "wrap",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "'M PLUS Rounded 1c', sans-serif",
+    }}
+  >
+    {props.status.status === "idle" && (
+      <>
+        コンパイル待機中
+        <CircularProgress size="1.5rem" sx={{ ml: "1rem" }} />
+      </>
+    )}
+    {props.status.status === "compile" && (
+      <>
+        コンパイル中
+        <CircularProgress size="1.5rem" sx={{ ml: "1rem" }} />
+      </>
+    )}
+    {props.status.status === "success" && (
+      <>
+        コンパイル完了
+        <CheckIcon color="success" />
+      </>
+    )}
+    {props.status.status === "error" && (
+      <>
+        コンパイル失敗
+        <ErrorOutlineIcon color="error" />
+        <Box width="100%">
+          <code>{props.status.error}</code>
+        </Box>
+      </>
+    )}
+  </Sheet>
+);
 
 const Log = (props: { log: string[] }) => {
   const scrollRef = useRef<HTMLDivElement>(null);
