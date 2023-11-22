fdf33e43e4addee53601049c624e5ba28caba141
tufusa <tufusa74@gmail.com>
2023-11-22 20:45:40 +0900

    fix: ビルド時にベースURLが正しく設定されない問題を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_URL,
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, "src/components"),
-      css: path.resolve(__dirname, "src/css"),
-      pages: path.resolve(__dirname, "src/pages"),
-      routes: path.resolve(__dirname, "src/routes"),
-      libs: path.resolve(__dirname, "src/libs"),
-      hooks: path.resolve(__dirname, "src/hooks"),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return {
+    plugins: [react()],
+    base: process.env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        css: path.resolve(__dirname, "src/css"),
+        pages: path.resolve(__dirname, "src/pages"),
+        routes: path.resolve(__dirname, "src/routes"),
+        libs: path.resolve(__dirname, "src/libs"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
     },
-  },
+  };
 });
