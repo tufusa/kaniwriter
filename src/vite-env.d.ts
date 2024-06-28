f64d8c5c2c7253a25a3a9c870e51a97360048899
tufusa <tufusa74@gmail.com>
2024-06-28 09:29:11 +0900

    chore: 環境変数の型定義を更新

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_COMPILER_URL: string;
   readonly VITE_BASE_URL: string;
   readonly VITE_WRITER_REPOSITORY_PATH: string;
+  readonly VITE_REFERENCE_URL: string;
 }
 
 interface ImpoerMeta {
