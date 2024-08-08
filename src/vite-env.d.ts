1667ed50041a2eb38f6faead9fb039cac38d6dcf
tufusa <tufusa74@gmail.com>
2024-08-08 16:07:51 +0900

    chore: デフォルトコンパイラバージョンを環境変数で書くように変更

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
   readonly VITE_WRITER_REPOSITORY_PATH: string;
   readonly VITE_REFERENCE_URL: string;
+  readonly VITE_COMPILER_VERSION_FALLBACK: string;
 }
 
 interface ImportMeta {
