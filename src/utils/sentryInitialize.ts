74528675d0618a3295503aa6565b2de83f36ffc7
kiharu <74781355+kiharu3112@users.noreply.github.com>
2025-07-09 23:56:47 +0900

    chore: sentry に報告するerrorをフィルタリング (#646)

--- src/utils/sentryInitialize.ts
+++ src/utils/sentryInitialize.ts
@@ -2,5 +2,12 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
+  ignoreErrors: [
+    "Already write mode.",
+    "No port.",
+    "The device has been lost.",
+    "Cannot write serial port.",
+    "Error excepted while writing.",
+  ],
   sendDefaultPii: true,
 });
